05fe97322b715e6a25f598cb9a209d11cdf5bf6d
othaderek <otha.hernandez@gmail.com>
2020-02-03 10:09:13 -0500

    hi annie

---
+++ app/models/artist.rb
@@ -0,0 +1,19 @@
+class Artist
+
+  attr_reader :name, :years_experience
+
+  @@all = []
+
+  def initialize(name, years_experience)
+    @name = name
+    @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
+end
